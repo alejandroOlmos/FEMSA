07410d95655ba2c214df1eaac42e54b52f350e2b
cristhian <alejandro.olmos@live.com>
2012-09-24 11:01:26 -0500

    Commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FEMSA::Application.config.secret_token = 'fdd2cb481d38c2722769b51b4b7b3a71b3e40155bc5b721b7263007f79a5c8c97aa5a23893509081d311256ddeb25ef6ad2955452e296fac0fc6e81795202b51'
